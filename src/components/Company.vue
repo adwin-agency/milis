3da2e807098ce5dd4a3f11a1661627578a662362
adwin-agency <adwin.developer@gmail.com>
2021-11-17 11:53:56 +0300

    discount banners

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -5,8 +5,8 @@
     <div class="company__row">
       <div class="company__image">
         <picture>
-          <source srcset="../assets/img/discount-banner-06-14-11-md.jpg" :media="'(min-width: ' + $breakpoints.md + 'px)'">        
-          <img src="../assets/img/discount-banner-06-14-11.jpg" alt="">
+          <source srcset="../assets/img/discount-banner-13-21-11-md.jpg" :media="'(min-width: ' + $breakpoints.md + 'px)'">        
+          <img src="../assets/img/discount-banner-13-21-11.jpg" alt="">
         </picture>
       </div>
       <div class="company__content">
