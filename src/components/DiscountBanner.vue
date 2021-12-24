da66510571ea020a8993f09b87075d333f0b2ed5
adwin-agency <adwin.developer@gmail.com>
2021-12-24 11:39:02 +0300

    discount banners

--- src/components/DiscountBanner.vue
+++ src/components/DiscountBanner.vue
@@ -8,7 +8,7 @@
           -47%
           <Icon name="leaf" class="discount-banner__icon" />
         </p>
-        <p class="discount-banner__date">20 - 23 декабря</p>
+        <p class="discount-banner__date">24 - 29 декабря</p>
       </div>
       <div
         v-if="$windowWidth >= $breakpoints.md"
