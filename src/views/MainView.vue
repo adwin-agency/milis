d4c069036597605525f7853a92ade6e1e154ac52
adwin-agency <adwin.developer@gmail.com>
2022-09-08 16:33:09 +0300

    main page set

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,90 +1,38 @@
 <template>
-  <Page>
-    <div class="v-main">
-      <div class="container">
-        <Header class="v-main__header" />
-        <MainBanner class="v-main__banner" />
-        <MainForm class="v-main__form" />
-        <MainReviews class="v-main__reviews" />
-        <MainPreview class="v-main__preview" />
-      </div>
-    </div>
-  </Page>
+  <div v-if="kitchens" class="v-main">
+    <Main class="v-main__main" :kitchens="kitchens" />
+  </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue'
-import MainBanner from '../components/MainBanner.vue'
-import MainForm from '../components/MainForm.vue'
-import MainPreview from '../components/MainPreview.vue'
-import MainReviews from '../components/MainReviews.vue'
-import Page from '../components/Page.vue'
+import Main from '../components/Main'
+import store from '@/store'
 
 export default {
   name: 'MainView',
   components: {
-    Page,
-    MainBanner,
-    Header,
-    MainForm,
-    MainReviews,
-    MainPreview
+    Main
+  },
+  computed: {
+    kitchens() {
+      return this.$store.state.mainKitchens
+    }
+  },
+  beforeRouteEnter(from, to, next) {
+    store.dispatch('loadMainKitchens')
+      .then(() => next())
   }
 }
 </script>
 
 <style lang="scss">
 .v-main {
-  padding-top: 20px;
-
-  &__banner {
-    margin-bottom: 44px;
-  }
-
-  &__form {
-    margin-bottom: 56px;
-  }
-
-  &__reviews {
-    margin-bottom: 32px;
-  }
-
-  &__preview {
-    margin-bottom: 30px;
-  }
-
-  @include media(lg) {
-    &__banner {
-      margin-bottom: 70px;
-    }
-
-    &__form {
-      margin-bottom: 64px;
-    }
-
-    &__reviews {
-      margin-bottom: 78px;
-    }
-
-    &__preview {
-      margin-bottom: 88px;
-    }
-  }
-
-  @include media(xl) {
-    padding-top: 30px;
-
-    &__banner {
-      margin-bottom: 90px;
-    }
-
-    &__form {
-      margin-bottom: 75px;
-    }
-
-    &__reviews {
-      margin-bottom: 90px;
-    }
+  &__main {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
--- src/views/_MainView.vue
+++ src/views/_MainView.vue
@@ -1,38 +1,90 @@
 <template>
-  <div v-if="kitchens" class="v-main">
-    <Main class="v-main__main" :kitchens="kitchens" />
-  </div>
+  <Page>
+    <div class="v-main">
+      <div class="container">
+        <Header class="v-main__header" />
+        <MainBanner class="v-main__banner" />
+        <MainForm class="v-main__form" />
+        <MainReviews class="v-main__reviews" />
+        <MainPreview class="v-main__preview" />
+      </div>
+    </div>
+  </Page>
 </template>
 
 <script>
-import Main from '../components/Main'
-import store from '@/store'
+import Header from '../components/Header.vue'
+import MainBanner from '../components/MainBanner.vue'
+import MainForm from '../components/MainForm.vue'
+import MainPreview from '../components/MainPreview.vue'
+import MainReviews from '../components/MainReviews.vue'
+import Page from '../components/Page.vue'
 
 export default {
   name: 'MainView',
   components: {
-    Main
-  },
-  computed: {
-    kitchens() {
-      return this.$store.state.mainKitchens
-    }
-  },
-  beforeRouteEnter(from, to, next) {
-    store.dispatch('loadMainKitchens')
-      .then(() => next())
+    Page,
+    MainBanner,
+    Header,
+    MainForm,
+    MainReviews,
+    MainPreview
   }
 }
 </script>
 
 <style lang="scss">
 .v-main {
-  &__main {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
+  padding-top: 20px;
+
+  &__banner {
+    margin-bottom: 44px;
+  }
+
+  &__form {
+    margin-bottom: 56px;
+  }
+
+  &__reviews {
+    margin-bottom: 32px;
+  }
+
+  &__preview {
+    margin-bottom: 30px;
+  }
+
+  @include media(lg) {
+    &__banner {
+      margin-bottom: 70px;
+    }
+
+    &__form {
+      margin-bottom: 64px;
+    }
+
+    &__reviews {
+      margin-bottom: 78px;
+    }
+
+    &__preview {
+      margin-bottom: 88px;
+    }
+  }
+
+  @include media(xl) {
+    padding-top: 30px;
+
+    &__banner {
+      margin-bottom: 90px;
+    }
+
+    &__form {
+      margin-bottom: 75px;
+    }
+
+    &__reviews {
+      margin-bottom: 90px;
+    }
   }
 }
 </style>
