a4845c0110353e2f98d6b8721c945e6b87c218f0
adwin-agency <adwin.developer@gmail.com>
2022-09-16 13:47:33 +0300

    promo date changes

--- src/components/Promo.vue
+++ src/components/Promo.vue
@@ -6,13 +6,15 @@
         <img src="../assets/img/discount-banner.jpg" alt="">
       </picture>
       <svg v-if="$windowWidth < $breakpoints.md" viewBox="0 0 360 80" class="promo__date">
-        <text v-if="promoText && sameMonth" x="320" y="48" text-anchor="end">{{ promoText.join(' - ') }}</text>
+        <!-- <text v-if="promoText && sameMonth" x="320" y="48" text-anchor="end">{{ promoText.join(' - ') }}</text>
         <text v-if="promoText && !sameMonth" x="320" y="30" text-anchor="end">{{ 'с\xa0' + promoText[0] }}</text>
-        <text v-if="promoText && !sameMonth" x="320" y="64" text-anchor="end">{{ 'по\xa0' + promoText[1] }}</text>
+        <text v-if="promoText && !sameMonth" x="320" y="64" text-anchor="end">{{ 'по\xa0' + promoText[1] }}</text> -->
+        <text v-if="promoText" x="320" y="50" text-anchor="end">{{ 'до\xa0' + promoText[1] }}</text>
       </svg>
       <svg v-else viewBox="0 0 360 80" class="promo__date">
-        <text v-if="promoText" x="38" y="32">{{ sameMonth ? promoText[0] + ' - ' + promoText[1].split('\xa0')[0] : 'с ' + promoText[0] }}</text>
-        <text v-if="promoText" x="38" y="68">{{ sameMonth ? promoText[1].split('\xa0')[1] : 'по ' + promoText[1] }}</text>
+        <!-- <text v-if="promoText" x="38" y="32">{{ sameMonth ? promoText[0] + ' - ' + promoText[1].split('\xa0')[0] : 'с ' + promoText[0] }}</text>
+        <text v-if="promoText" x="38" y="68">{{ sameMonth ? promoText[1].split('\xa0')[1] : 'по ' + promoText[1] }}</text> -->
+        <text v-if="promoText" x="38" y="32">{{ 'до\xa0' + promoText[1] }}</text>
       </svg>
     </div>
   </div>
